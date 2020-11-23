768d1565ac7152aff19fff9ad2c8e344fa61c5cd
kote-janjgava <66737757+kote-janjgava@users.noreply.github.com>
2020-11-23 19:23:19 +0400

    commit

---
+++ credentials/firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCxV0SDdoEwGbwsKStFI2ymq7e-b_E2CM4",
+  authDomain: "tsnobari.firebaseapp.com",
+  databaseURL: "https://tsnobari.firebaseio.com",
+  projectId: "tsnobari",
+  storageBucket: "tsnobari.appspot.com",
+  messagingSenderId: "19248344220",
+  appId: "1:19248344220:web:abd515ced02646c5669159",
+  measurementId: "G-0D0S6JLSDY"
+};
+
+export { firebaseConfig };
